972406e1e4f3203d0f11709433d17f279a66e848
iamhaao <haolee2002tst@gmail.com>
2024-04-21 21:06:24 +0700

    hidden some fields

--- src/products/dto/products.dto.ts
+++ src/products/dto/products.dto.ts
@@ -1,4 +1,4 @@
-import { Expose, Transform, Type } from 'class-transformer';
+import { Exclude, Expose, Transform, Type } from 'class-transformer';
 
 export class ProductsDTO {
   @Expose()
@@ -17,28 +17,58 @@ export class ProductList {
 
   @Expose({ name: 'product_title' })
   title: string;
+
   @Expose({ name: 'product_description' })
   description: string;
+
   @Expose({ name: 'product_price' })
   price: number;
+
   @Expose({ name: 'product_stock' })
   stock: number;
+
   @Expose({ name: 'product_images' })
   @Transform(({ value }) => value.toString().split(','))
   images: string[];
-  @Transform(({ obj }) => {
-    return {
-      id: obj.category_id,
-      title: obj.category_title,
-    };
-  })
+
+  @Transform(({ obj }) => ({
+    id: obj.category_id,
+    title: obj.category_title,
+  }))
   @Expose()
   category: any;
-  //   @Expose({ name: 'category_title' })
-  //   category: string;
+
   @Expose({ name: 'reviewCount' })
   review: number;
 
   @Expose({ name: 'avgRating' })
   rating: number;
+
+  // Ẩn các trường không cần thiết
+  @Exclude({ toClassOnly: true })
+  product_createdAt: string;
+
+  @Exclude({ toClassOnly: true })
+  product_updatedAt: string;
+
+  @Exclude({ toClassOnly: true })
+  product_addedById: number;
+
+  @Exclude({ toClassOnly: true })
+  product_categoryId: number;
+
+  @Exclude({ toClassOnly: true })
+  category_id: number;
+
+  @Exclude({ toClassOnly: true })
+  category_description: string;
+
+  @Exclude({ toClassOnly: true })
+  category_createdAt: string;
+
+  @Exclude({ toClassOnly: true })
+  category_updatedAt: string;
+
+  @Exclude({ toClassOnly: true })
+  category_addedById: number;
 }
